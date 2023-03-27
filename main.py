ea2b1912a92ac1f09a0ff1d0939a2146f1d4d714
Arosaleskdoce <arosales@kdoce.cl>
2023-03-27 10:49:20 -0300

    A.R inicializacion

---
+++ main.py
@@ -0,0 +1,14 @@
+#importamos random y string para los caracteres del generador
+import random
+import string
+
+print("Password ")
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+chars = lower + upper + num + symbols
+
+temp = random.sample(chars, 25)
+print("".join(temp))
